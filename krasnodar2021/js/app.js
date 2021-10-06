913f4e60157255b20e6f6e240b99886330581878
Dima <70588360+WebOriginals@users.noreply.github.com>
2021-10-06 21:31:18 +0300

    drag & drop

--- krasnodar2021/js/app.js
+++ krasnodar2021/js/app.js
@@ -556,6 +556,7 @@ dragItems.forEach(dragItem => {
 })
 
 function handlerDragstart(event) {
+    event.dataTransfer.setData("aside-transport", this.dataset.itemId);
     this.classList.add('active');
 }
 
@@ -572,16 +573,48 @@ function handlerDrag(event) {
 dragZones.forEach(dragZone => {
     dragZone.addEventListener('dragenter', handlerDragenter);
     dragZone.addEventListener('dragleave', handlerDragleave);
+    dragZone.addEventListener('dragover', handlerDragover);
+    dragZone.addEventListener('drop', handlerDrop);
 })
 
 function handlerDragenter(event) {
-    console.log("dragenter", this);
+    event.preventDefault();
+    console.log('dragenter', this);
     this.classList.add('table-grid-registry-blue');
 }
 
 function handlerDragleave(event) {
-    console.log("dragleave", this);
+    event.preventDefault();
+    console.log('dragleave', this);
     this.classList.remove('table-grid-registry-blue');
+}
+
+function handlerDragover(event) {
+    event.preventDefault();
+}
+
+function handlerDrop(event) {
+    const dragFlag = event.dataTransfer.getData("aside-transport");
+    const dragItem = document.querySelector(`[data-item-id="${dragFlag}"]`)
+    const dragItemName = dragItem.querySelector('.aside-transport__name').textContent;
+    const dragItemStatus = dragItem.querySelector('.aside-transport__status').textContent.trim();
+    const zonaId = this.dataset.id;
+    const zonaName = this.querySelector(".table-bottom-row__element:nth-child(4) b");
+    const zonaStatus = this.querySelector(".table-bottom-row__element:nth-child(5)");
+    this.classList.remove('table-grid-registry-blue');
+    if(zonaStatus.textContent === "Утверждена" || zonaStatus.textContent === "Забронирован"){
+        return;
+    }
+    if(dragItemStatus === "Занят" || dragItemStatus === "Занят начальником"){
+        this.classList.add('table-grid-registry-ellowe');
+        zonaName.textContent = dragItemName;
+        zonaStatus.textContent = dragItemStatus;
+    }
+    if(dragItemStatus === "Свободен"){
+        this.classList.add('table-grid-registry-green');
+        zonaName.textContent = dragItemName;
+        zonaStatus.textContent = dragItemStatus;
+    }
 }
 
 //slider
